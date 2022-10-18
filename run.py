6429b16371ab004d3bf37a8063796b4421505ab7
frirsta <frista-8@hotmail.com>
2022-10-18 07:54:49 +0000

    Add play_game function to play hangman, function gets word from get_valid_word function and set it as the game word, function stores the guessed letters and guessed words in case user guess it twice. The play_game function takes the user input and look if the guessed letter is in the word

--- run.py
+++ run.py
@@ -22,9 +22,46 @@ SHEET = GSPREAD_CLIENT.open("python_hangman")
 high_scores = SHEET.worksheet('high_scores')
 
 
-def get_valid_word(word_data):
-    word = random.choice(words)
+def get_valid_word(words_data):
+    word = random.choice(words_data)
     while '-' in word or ' ' in word:
-        word = random.choice(word_data)
+        word = random.choice(words_data)
     return word.upper()
 
+
+def play_game():
+    word = get_valid_word(words)
+    word_letters = set(word) # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    guessed_letters = set()
+    guessed_words = set()
+    lives = 10
+
+    while len(word_letters) > 0 and lives > 0:
+        print('You have used theese letters: ', ' '.join(guessed_letters))
+
+        word_list = [letter if letter in guessed_letters else '_' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+        user_input = input('Guess the full word or with one letter:  \n').upper()
+        print(user_input)
+        if len(user_input) == 1:
+            if user_input in alphabet - guessed_letters:
+                guessed_letters.add(user_input)
+                if user_input in word_letters:
+                    word_letters.remove(user_input)
+                    print('Correct letter')
+            elif user_input in guessed_letters:
+                print('You have already guessed this letter')
+            else:
+                print("Invalid characters, try again!")
+
+
+
+play_game()
+# elif len(user_input) == len(full_word) and user_input is alpha():
+#         if user_input in guessed_words:
+#             print('You have already guessed this word!')
+#             else:
+#                 lives = - 1
+#                 print(f'The letter is not in the word. You have {lives}lives left!')
